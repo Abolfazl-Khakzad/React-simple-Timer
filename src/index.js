ca5629dc5b0235173c9e9ce488769359e0a2b7a5
abolfazl <a.khakzad1993@gmail.com>
2022-09-12 08:20:01 +0200

    renew

--- src/index.js
+++ src/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Component } from 'react';
 
+class App extends Component {
+  render() { 
+    return (
+    
+        <div>
+        
+        <h1>Timer</h1>
+        <h2>
+          {
+          new Date().toLocaleTimeString()
+          }
+        </h2>
+        </div>
+    )
+  }
+}
+ 
+export default App;
 
 
 const tick=()=>{
-  let elem=(
-    <div>
-    
-    <h1>Timer</h1>
-    <h2>
-      {
-        new Date().toLocaleTimeString()
-      }
-    </h2>
-
-    </div>
-  )
-  ReactDOM.render( elem,document.getElementById('root'));
+  
+  ReactDOM.render(<App/> , document.getElementById('root'));
 }
+
 setInterval(() => {
   tick();
 }, 1000);
-
