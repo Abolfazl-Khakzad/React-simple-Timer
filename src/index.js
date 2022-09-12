4ee6722f3a0c5106fdb79bf01b705e198679650f
abolfazl <a.khakzad1993@gmail.com>
2022-09-12 07:05:02 +0200

    timer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const tick=()=>{
+  let elem=(
+    <div>
+    
+    <h1>Timer</h1>
+    <h2>
+      {
+        new Date().toLocaleTimeString()
+      }
+    </h2>
+
+    </div>
+  )
+  ReactDOM.render( elem,document.getElementById('root'));
+}
+setInterval(() => {
+  tick();
+}, 1000);
+
